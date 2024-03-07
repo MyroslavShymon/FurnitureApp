5e3d6a96b1a399e4f261a90eabbc339e100e0a83
Artem Matok <artemmatok@gmail.com>
2024-03-07 12:53:09 +0200

    Add Connection DB (task #4)

--- FurnitureProjectTeam/FurnitureProjectTeam/Client/obj/Debug/net7.0/FurnitureProjectTeam.Client.AssemblyInfo.cs
+++ FurnitureProjectTeam/FurnitureProjectTeam/Client/obj/Debug/net7.0/FurnitureProjectTeam.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FurnitureProjectTeam.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdb3b647dcead66717303daf7aedd1e9f6d34d0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FurnitureProjectTeam.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FurnitureProjectTeam.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ FurnitureProjectTeam/FurnitureProjectTeam/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FurnitureProjectTeam.Server.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+            
+        }
+    }
+}
